0472de4afda8ea07d3f3b150ea9a6f55149ba542
Vivek Thakyal <vivek.thakyal@gmail.com>
2013-03-06 23:30:07 +0530

    Converted all tabs to spaces

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -5,20 +5,20 @@
  * value - the value associated with the key
  */
 function Node(key, value) {
-	this.key_ = key;
-	this.value_ = value;
-	this.left_ = null;
-	this.right_ = null;
+    this.key_ = key;
+    this.value_ = value;
+    this.left_ = null;
+    this.right_ = null;
 
-	this.size_ = 1;
-	this.height_ = 0;
+    this.size_ = 1;
+    this.height_ = 0;
 }
 
 /**
  * Creates a binary search tree object with a null root
  */
 function BinarySearchTree() {
-	this.root_ = null;
+    this.root_ = null;
 }
 
 /**
@@ -29,11 +29,11 @@ function BinarySearchTree() {
  * value - the value associated with the key
  */
 BinarySearchTree.prototype.insert = function(key, value) {
-	this.root_ = insertImpl_(this.root_, key, value);
+    this.root_ = insertImpl_(this.root_, key, value);
 };
 
 BinarySearchTree.prototype.remove = function(key, value) {
-	// TODO Hibbard deletion
+    // TODO Hibbard deletion
 };
 
 /**
@@ -42,25 +42,25 @@ BinarySearchTree.prototype.remove = function(key, value) {
  * key - the key to search in the BST
  */
 BinarySearchTree.prototype.containsKey = function(key) {
-	var curr = this.root_;
+    var curr = this.root_;
 
-	while (curr !== null) {
-		if ( key < curr.key_) {
-			curr = curr.left_;
-		} else if (key > curr.key_) {
-			curr = curr.right_;
-		} else {
-			return true;
-		}
-	}
-	return false;
+    while (curr !== null) {
+        if ( key < curr.key_) {
+            curr = curr.left_;
+        } else if (key > curr.key_) {
+            curr = curr.right_;
+        } else {
+            return true;
+        }
+    }
+    return false;
 };
 
 /**
  * Returns the height of the BST
  */
 BinarySearchTree.prototype.height_ = function () {
-	return heightImpl_(this.root_);
+    return heightImpl_(this.root_);
 };
 
 /**
@@ -72,88 +72,88 @@ BinarySearchTree.prototype.height_ = function () {
  * height - height of the canvas
  */
 BinarySearchTree.prototype.draw = function (ctxFg, ctxBg, width, height, spreadNodes) {
-	var minX = width * 0.05;
-	var maxX = width - minX;
-	var minY = height * 0.05;
-	var maxY = height - minY;
+    var minX = width * 0.05;
+    var maxX = width - minX;
+    var minY = height * 0.05;
+    var maxY = height - minY;
 
-	ctxBg.beginPath();
-	drawImpl_(ctxFg, ctxBg, this.root_, minX, maxX, minY, (maxY - minY)/this.height_(), spreadNodes);
-	ctxBg.stroke();
-	ctxBg.closePath();
+    ctxBg.beginPath();
+    drawImpl_(ctxFg, ctxBg, this.root_, minX, maxX, minY, (maxY - minY)/this.height_(), spreadNodes);
+    ctxBg.stroke();
+    ctxBg.closePath();
 };
 
 function drawImpl_(ctxFg, ctxBg, node, xMin, xMax, y, yIncr, spreadNodes) {
-	var xSplit = xMin + (xMax - xMin) * (spreadNodes ? sizeWeightedRatio_(node) : 0.5);
-	var x = (xMax + xMin) / 2;
-	ctxFg.beginPath();
-	ctxFg.fillStyle = '#306BA2';
-	ctxFg.arc(x, y, 10, 0, Math.PI * 2, true);
-	ctxFg.fill();
-	ctxFg.stroke();
-	ctxFg.fillStyle = '#FFFFFF';
-	ctxFg.font = 'bold 12px serif';
+    var xSplit = xMin + (xMax - xMin) * (spreadNodes ? sizeWeightedRatio_(node) : 0.5);
+    var x = (xMax + xMin) / 2;
+    ctxFg.beginPath();
+    ctxFg.fillStyle = '#306BA2';
+    ctxFg.arc(x, y, 10, 0, Math.PI * 2, true);
+    ctxFg.fill();
+    ctxFg.stroke();
+    ctxFg.fillStyle = '#FFFFFF';
+    ctxFg.font = 'bold 12px serif';
 
-	// the numbers used for positioning the text are a result of guided hit and trial.
-	// I need a better way to auto calculate the position based on the font-family and
-	// font-size
-	ctxFg.fillText(node.key_, x - node.key_.toString().length * 6/2, y + 9/2);
-	ctxFg.closePath();
+    // the numbers used for positioning the text are a result of guided hit and trial.
+    // I need a better way to auto calculate the position based on the font-family and
+    // font-size
+    ctxFg.fillText(node.key_, x - node.key_.toString().length * 6/2, y + 9/2);
+    ctxFg.closePath();
 
-	ctxBg.lineTo(x, y);
+    ctxBg.lineTo(x, y);
 
-	if (node.left_ !== null) {
-		ctxBg.moveTo(x, y);
-		drawImpl_(ctxFg, ctxBg, node.left_, xMin, xSplit, y + yIncr, yIncr, spreadNodes);
-	}
+    if (node.left_ !== null) {
+        ctxBg.moveTo(x, y);
+        drawImpl_(ctxFg, ctxBg, node.left_, xMin, xSplit, y + yIncr, yIncr, spreadNodes);
+    }
 
-	if (node.right_ !== null) {
-		ctxBg.moveTo(x, y);
-		drawImpl_(ctxFg, ctxBg, node.right_, xSplit, xMax, y + yIncr, yIncr, spreadNodes);
-	}
+    if (node.right_ !== null) {
+        ctxBg.moveTo(x, y);
+        drawImpl_(ctxFg, ctxBg, node.right_, xSplit, xMax, y + yIncr, yIncr, spreadNodes);
+    }
 }
 
 function sizeWeightedRatio_(node) {
-	if (nodeSize_(node.left_) == nodeSize_(node.right_)) {
-		return 0.5;
-	}
+    if (nodeSize_(node.left_) == nodeSize_(node.right_)) {
+        return 0.5;
+    }
 
-	// return a non-zero ratio if the left child is null, otherwise subsequent nodes
-	// with a null left child will get rendered right on the left bound all one directly
-	// under the other vertically
-	var alpha = 0.2; return node.left_ === null ? alpha : (nodeSize_(node.left_) / nodeSize_(node));
+    // return a non-zero ratio if the left child is null, otherwise subsequent nodes
+    // with a null left child will get rendered right on the left bound all one directly
+    // under the other vertically
+    var alpha = 0.2; return node.left_ === null ? alpha : (nodeSize_(node.left_) / nodeSize_(node));
 }
 
 function insertImpl_(node, key, value) {
-	if (node === null) {
-		node = new Node(key, value);
-		return node;
-	}
+    if (node === null) {
+        node = new Node(key, value);
+        return node;
+    }
 
-	if (key < node.key_) {
-		node.left_ = insertImpl_(node.left_, key, value);
-	} else if (key > node.key_) {
-		node.right_ = insertImpl_(node.right_, key, value);
-	} else {
-		node.value_ = value;
-	}
+    if (key < node.key_) {
+        node.left_ = insertImpl_(node.left_, key, value);
+    } else if (key > node.key_) {
+        node.right_ = insertImpl_(node.right_, key, value);
+    } else {
+        node.value_ = value;
+    }
 
-	node.size_ = nodeSize_(node.left_) + nodeSize_(node.right_) + 1;
-	node.height_ = Math.max(nodeHeight_(node.left_), nodeHeight_(node.right_)) + 1;
+    node.size_ = nodeSize_(node.left_) + nodeSize_(node.right_) + 1;
+    node.height_ = Math.max(nodeHeight_(node.left_), nodeHeight_(node.right_)) + 1;
 
-	return node;
+    return node;
 }
 
 
 function heightImpl_(node) {
-	if (node === null) return 0;
-	return Math.max(heightImpl_(node.left_), heightImpl_(node.right_)) + 1;
+    if (node === null) return 0;
+    return Math.max(heightImpl_(node.left_), heightImpl_(node.right_)) + 1;
 }
 
 function nodeSize_(node) {
-	return node === null ? 0 : node.size_;
+    return node === null ? 0 : node.size_;
 }
 
 function nodeHeight_(node) {
-	return node === null ? 0 : node.height_;
+    return node === null ? 0 : node.height_;
 }
--- visualization.js
+++ visualization.js
@@ -1,51 +1,51 @@
 var SIZE_WEIGHTED = true;
 
 function init() {
-	var canvasFg = document.getElementById('layerFg');
-	var canvasBg = document.getElementById('layerBg');
+    var canvasFg = document.getElementById('layerFg');
+    var canvasBg = document.getElementById('layerBg');
 
-	canvasFg.width = canvasBg.width = canvasFg.offsetWidth;
-	canvasFg.height = canvasBg.height = canvasFg.offsetHeight;
+    canvasFg.width = canvasBg.width = canvasFg.offsetWidth;
+    canvasFg.height = canvasBg.height = canvasFg.offsetHeight;
 
-	if (canvasFg.getContext) {
-		var ctxFg = canvasFg.getContext('2d');
-		var ctxBg = canvasBg.getContext('2d');
+    if (canvasFg.getContext) {
+        var ctxFg = canvasFg.getContext('2d');
+        var ctxBg = canvasBg.getContext('2d');
 
-		ctxFg.fillStyle = '#7A1F1F';
-		ctxFg.strokeStyle = '#CBCBCB';
-		ctxFg.lineWidth = 3;
-		var tree = generateBST();
-		tree.draw(ctxFg, ctxBg, canvasFg.width, canvasFg.height, SIZE_WEIGHTED);
-	}
+        ctxFg.fillStyle = '#7A1F1F';
+        ctxFg.strokeStyle = '#CBCBCB';
+        ctxFg.lineWidth = 3;
+        var tree = generateBST();
+        tree.draw(ctxFg, ctxBg, canvasFg.width, canvasFg.height, SIZE_WEIGHTED);
+    }
 
-	document.getElementById('reset').onclick = function() {
-		ctxFg.clearRect(0, 0, canvasFg.width, canvasFg.height);
-		ctxBg.clearRect(0, 0, canvasBg.width, canvasBg.height);
-		tree = generateBST();
-		tree.draw(ctxFg, ctxBg, canvasFg.width, canvasFg.height, SIZE_WEIGHTED);
-	};
+    document.getElementById('reset').onclick = function() {
+        ctxFg.clearRect(0, 0, canvasFg.width, canvasFg.height);
+        ctxBg.clearRect(0, 0, canvasBg.width, canvasBg.height);
+        tree = generateBST();
+        tree.draw(ctxFg, ctxBg, canvasFg.width, canvasFg.height, SIZE_WEIGHTED);
+    };
 
-	document.getElementById('toggleWeighted').onclick = function() {
-		SIZE_WEIGHTED = !SIZE_WEIGHTED;
-		ctxFg.clearRect(0, 0, canvasFg.width, canvasFg.height);
-		ctxBg.clearRect(0, 0, canvasBg.width, canvasBg.height);
-		tree.draw(ctxFg, ctxBg, canvasFg.width, canvasFg.height, SIZE_WEIGHTED);
-	};
+    document.getElementById('toggleWeighted').onclick = function() {
+        SIZE_WEIGHTED = !SIZE_WEIGHTED;
+        ctxFg.clearRect(0, 0, canvasFg.width, canvasFg.height);
+        ctxBg.clearRect(0, 0, canvasBg.width, canvasBg.height);
+        tree.draw(ctxFg, ctxBg, canvasFg.width, canvasFg.height, SIZE_WEIGHTED);
+    };
 }
 
 function generateBST() {
-	var input = generateRandomArray(64, 0, 100);
-	var tree = new BinarySearchTree();
-	for(var i = 0; i < input.length; i++) {
-		tree.insert(input[i], input[i]);
-	}
-	return tree;
+    var input = generateRandomArray(64, 0, 100);
+    var tree = new BinarySearchTree();
+    for(var i = 0; i < input.length; i++) {
+        tree.insert(input[i], input[i]);
+    }
+    return tree;
 }
 
 function generateRandomArray(size, lo, hi) {
-	var arr = [];
-	for (var i = 0; i < size; i++) {
-		arr.push((Math.floor(Math.random() * (hi - lo + 1)) + lo));
-	}
-	return arr;
+    var arr = [];
+    for (var i = 0; i < size; i++) {
+        arr.push((Math.floor(Math.random() * (hi - lo + 1)) + lo));
+    }
+    return arr;
 }
