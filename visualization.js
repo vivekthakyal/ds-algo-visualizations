713131c97ff825266d2d1c9e6f864df4a847510a
Vivek Thakyal <vivek.thakyal@gmail.com>
2013-03-10 20:19:01 +0530

    Added a field for manual input to create the tree

--- visualization.js
+++ visualization.js
@@ -1,11 +1,13 @@
 var SIZE_WEIGHTED = true;
 
-function init() {
+$(document).ready(function() {
     var canvasFg = document.getElementById('layerFg');
     var canvasBg = document.getElementById('layerBg');
 
     canvasFg.width = canvasBg.width = canvasFg.offsetWidth;
     canvasFg.height = canvasBg.height = canvasFg.offsetHeight;
+
+    var tree = generateBST();
 
     if (canvasFg.getContext) {
         var ctxFg = canvasFg.getContext('2d');
@@ -13,31 +15,46 @@ function init() {
 
         ctxFg.lineWidth = 1.5;
         ctxBg.lineWidth = 1.5;
-        var tree = generateBST();
         tree.draw(ctxFg, ctxBg, canvasFg.width, canvasFg.height, SIZE_WEIGHTED);
     }
 
-    document.getElementById('reset').onclick = function() {
-        ctxFg.clearRect(0, 0, canvasFg.width, canvasFg.height);
-        ctxBg.clearRect(0, 0, canvasBg.width, canvasBg.height);
+    $('#treeKeysWrapper').hide();
+
+    $('#treeKeys').keyup(function(event) {
+        var keys = $(this).val().split(' ');
+
+        tree = new vktl.rbt.RedBlackTree();
+        for (var i = 0; i < keys.length; i++) {
+            if (keys[i] !== null & keys[i].length > 0) {
+                tree.insert(parseInt(keys[i], 10), parseInt(keys[i], 10));
+            }
+        }
+        tree.draw(ctxFg, ctxBg, canvasFg.width, canvasFg.height, SIZE_WEIGHTED);
+    });
+
+    $('#manualInput').click(function() {
+        $('#treeKeysWrapper').slideDown();
+        $('#treeKeys').val('').focus();
+        tree = new vktl.rbt.RedBlackTree();
+        tree.draw(ctxFg, ctxBg, canvasFg.width, canvasFg.height, SIZE_WEIGHTED);
+    });
+
+    $('#reset').click(function() {
         tree = generateBST();
         tree.draw(ctxFg, ctxBg, canvasFg.width, canvasFg.height, SIZE_WEIGHTED);
-    };
-
-    document.getElementById('toggleWeighted').onclick = function() {
-        SIZE_WEIGHTED = !SIZE_WEIGHTED;
-        ctxFg.clearRect(0, 0, canvasFg.width, canvasFg.height);
-        ctxBg.clearRect(0, 0, canvasBg.width, canvasBg.height);
-        tree.draw(ctxFg, ctxBg, canvasFg.width, canvasFg.height, SIZE_WEIGHTED);
-    };
-}
+    });
+});
 
 function generateBST() {
-    var input = generateRandomArray(64, 0, 150);
+    var input = generateRandomArray(48, 0, 150);
     var tree = new vktl.rbt.RedBlackTree();
+    var text = '';
     for(var i = 0; i < input.length; i++) {
+        text += input[i] + ' ';
         tree.insert(input[i], input[i]);
     }
+    $('#treeKeys').val(text);
+
     return tree;
 }
 
