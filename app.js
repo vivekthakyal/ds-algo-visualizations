04403a29f06aa9de6b8cec3643aaee5100150c73
Vivek Thakyal <vivek.thakyal@gmail.com>
2015-03-19 20:46:10 +0530

    The server now uses process.env.PORT for the http port if it is available, else it defaults to port 3000

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ app.get('/', function (req, res) {
   res.sendFile('index.html', {root: __dirname });
 });
 
-var server = app.listen(3000, function () {
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
